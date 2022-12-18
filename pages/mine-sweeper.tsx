e66428169cf4f4f5a775ce35a8c920ec5b173149
soundobj <soundobj@yahoo.com>
2022-12-18 13:19:14 +0000

    feat: initial minesweeper layout

--- pages/mine-sweeper.tsx
+++ pages/mine-sweeper.tsx
@@ -1,15 +1,36 @@
 import Head from 'next/head'
 import { useRef, useState } from 'react';
+import { generateBoard, placeMines } from '@/components/MineSweeper/utils';
 
 export default function MineSweeper() {
+  const [board, setBoard] = useState(placeMines(generateBoard(10), 10));
+
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [isGameWon, setIsGameWon] = useState(false);
+
+  console.log(board);
 
   return (
-    <div className='container md mx-auto h-full'>
+    <div className=''>
       <Head>
         <title>MineSweeper</title>
       </Head>
-      <main className='ml-3 mr-3'>
-    foo
+      <main className='box-border'>
+        <div className='grid grid-cols-10 max-w-lg m-auto gap-1'>
+          {board.map((row, i) => (
+            <>
+              {row.map((cell, j) => (
+                <div key={`cell_${i}_${j}`} className={`bg-gray-300 relative w-full justify-self-center border-black border before:h-0 before:inline-block before:pt-[100%] before:relative before:w-[1px]`}>
+                  <div className='absolute top-0 left-0 bottom-0 w-full grid justify-items-start align-items'>
+                    <div className='p-8'>
+                      <h2>{cell}</h2>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </>
+          ))}
+        </div>
       </main>
     </div>
   )
